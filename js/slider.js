ffde949c78728e8ff01d08a626536f817a9371e8
WISBoyush <108056116+WISBoyush@users.noreply.github.com>
2022-07-21 00:09:27 +0300

    pre-final commit

--- js/slider.js
+++ js/slider.js
@@ -7,11 +7,30 @@ window.addEventListener('load', () => {
   const slider = document.querySelector('.slider__container-slides');
   const images = document.querySelectorAll('.slider__container-slides img');
 
+  // slider container
+
+  const container = document.querySelector('.wrapper__right-content');
+  const slider_content = container.querySelector('.body__slider-content');
+  const main_content = document.querySelector('.wrapper__left-content');
+  const wrapper = document.querySelector('.wrapper');
+  const before_elem = document.querySelector('.header');
+
+  if (window.innerWidth <= 1410) {
+  wrapper.removeChild(container);
+  before_elem.after(slider_content);
+  main_content.style.flexBasis = '100%';
+}
+  
+  
   // counter
   let counter = 0
   const image_width = images[0].clientWidth;
 
-  slider.style.transform = `translateX(${-image_width * counter}px)`;
+
+
+  // container.style.flexBasis = `${image_width}px`;
+
+  // slider.style.transform = `translateX(${-image_width * counter}px)`;
 
   right.addEventListener('click', () => {
     counter >= images.length - 1? counter = -1: null;
